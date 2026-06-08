78080a01a4bace38484dea1ee6cbf53c295bfee6
Osmar23-92 <osmar.c.ealr@gmail.com>
2026-06-08 16:47:19 -0300

    Add files via upload

---
+++ aula1/java-t/ExemploAula1.java
@@ -0,0 +1,11 @@
+public class ExemploAula1 {
+	public static void main(String args[]){
+		System.out.println("Hellow world!");
+
+		String nome = "Osmar c.Rosa";
+		int idade = 34;
+
+		System.out.println("Bem vindo " + nome);
+		System.out.println("Idade: " + idade + "anos");
+	}
+}
