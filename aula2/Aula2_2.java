f82d841bb1eba7850ed4633bc0616b1fdb2963d5
Osmar23-92 <osmar.c.ealr@gmail.com>
2026-06-10 16:47:57 -0300

    Add files via upload

--- aula2/Aula2_2.java
+++ aula2/Aula2_2.java
@@ -1,4 +1,22 @@
-public static void main (String args[]){
-	System
+import java.util.Scanner;
 
+public class aula2_2 {
+	public static void main (String args[]){
+
+	Scanner s = new Scanner(System.in);
+
+
+	   System.out.println("Digite a 1° nota:");
+	   float n1 = s.nextFloat();
+
+	   System.out.println("Digite a 2° nota:");
+	   float n2 = s.nextFloat();
+
+	  float result = (n1 + n2) / 2;
+
+	   (result >= 120 ) {
+	    System.out.println("Passou");
+	    } else {System.out.println("Rodou...");
+}
+  }
 }
